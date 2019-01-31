ece8eeeede9d0f2007d8549aaf4f6e1811e59e18
Julien Richoz <julienrichoz@outlook.com>
2019-01-31 14:08:59 +0100

    Configure Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
-const path = require('path');
-
 module.exports = {
   entry: './src/PokedashGame.js',
   resolve: {
     extensions: ['.js']
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'main.mjs',
+    path: __dirname
   }
 }
