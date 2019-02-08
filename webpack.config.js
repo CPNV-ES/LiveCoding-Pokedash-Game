4f148c92d14f8dbde949a3b2e2d5f6d74ead9651
Julien Richoz <julienrichoz@outlook.com>
2019-02-08 11:31:02 +0100

    Update dependence

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,24 @@ module.exports = {
       files: {
         "game.mjs": [
           // Specify files to merge into the main file
-          'src/main.js',
-          'src/PokedashGame.js'
+          // p5.js
+          'node_modules/p5/lib/p5.min.js',
+          'node_modules/p5/lib/addons/p5.dom.min.js',
+          'node_modules/p5/lib/addons/p5.sound.min.js',
+
+          // Pikachu Classes
+          'src/elements/Element.js',
+          'src/elements/StaticElement.js',
+          'src/elements/MovableElement.js',
+          'src/elements/Boulder.js',
+          'src/elements/Door.js',
+          'src/elements/Protagonist.js',
+          'src/elements/Pokeball.js',
+          'src/elements/Road.js',
+          'src/elements/Tree.js',
+          'src/DynamicElement.js',
+          'src/PokedashGame.js',
+          'src/main.js'
         ]
       }
     })
