40f2fb57e18fb888f0a282b6b5bdc46540c2f5ef
bastien <bastien.nicoud@gmail.com>
2019-02-14 18:49:27 +0100

    Game class example

--- dist/game.js
+++ dist/game.js
@@ -5,14 +5,20 @@ class TestClass {
   }
 }
 
-console.warn('Game module loaded');
+console.log('Game module loaded');
 
 class Game {
 
+  /**
+   * 
+   * @param {HTMLElement} el game base element
+   * @param {String} assetsBasePath 
+   */
   constructor(el, assetsBasePath) {
 
     console.log('Game class called');
     this.el = el;
+    this.el.style.backgroundColor = 'red';
     this.assetsBasePath = assetsBasePath;
 
     console.info(this.el);
--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,19 @@
 import { TestClass } from './TestClass'
 
-console.warn('Game module loaded')
+console.log('Game module loaded')
 
 export class Game {
 
+  /**
+   * 
+   * @param {HTMLElement} el game base element
+   * @param {String} assetsBasePath 
+   */
   constructor(el, assetsBasePath) {
 
     console.log('Game class called')
     this.el = el
+    this.el.style.backgroundColor = 'red'
     this.assetsBasePath = assetsBasePath
 
     console.info(this.el)
