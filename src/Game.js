ea2ef6b18e58333d68e0266f3b5ed068f3701a17
Julien Richoz <julienrichoz@outlook.com>
2019-02-15 14:31:37 +0100

    Add game comportments when keys pressed

--- src/Game.js
+++ src/Game.js
@@ -106,11 +106,90 @@ export class Game {
     }      
   }
 
-  keyPressed () {
-    console.warn(this.sketch.keyCode)
-    if (this.sketch.keyCode === this.sketch.LEFT_ARROW){
-      console.warn('leftarrow pressed')
+  // Get element from protagonist
+  getElement(direction, distance, optionnal){
+    if (distance < 0) return false
+    
+    let x = this.protagonist.posX
+    let y = this.protagonist.posY
+    let element
+    
+    if (direction === 'left' || direction === this.sketch.LEFT_ARROW){
+        if (x < 0 ) return false // If it's out of the map
+        element = this.mapElement[x - distance][y]
     }
+
+    else if (direction === 'right' || direction === this.sketch.RIGHT_ARROW){
+        if (x >= this.blockWidth + distance) return false // If it's out of the map
+        element = this.mapElement[x + distance][y]
+    }
+
+    else if (direction === 'up' || direction === this.sketch.UP_ARROW){
+        if (y < 0 ) return false // If it's out of the map
+        element = this.mapElement[x][y - distance]
+    }
+
+    else if (direction === 'down' || direction === this.sketch.DOWN_ARROW){
+        if (y >= this.blockHeight + distance) return false // If it's out of the map
+        element = this.mapElement[x][y + distance]
+    }
+    else return null
+
+    if(optionnal == 'admin') return element
+
+    return element.constructor.name.toString() // Return the name of the class's element in string
+  }
+
+  refreshPos(){
+      for (let y = 0; y < this.columns; y++){
+          for (let x = 0; x < this.rows; x++) {
+              // Refresh pos X and Y
+              this.mapElement[x][y].x = x * this.blockWidth
+              this.mapElement[x][y].y = y * this.blockHeight
+          }
+      }
+  }
+
+  keyPressed() {
+      //let element = null
+      let element = this.getElement(this.sketch.keyCode, 1)
+      console.log('element: ' + element)
+      if(element == 'road') return false
+      if (this.sketch.keyCode === this.sketch.LEFT_ARROW){
+          // Check if not going out of the map
+          if(this.protagonist.posX <= 0) return false 
+          //element = this.getElement(this.sketch.keyCode, 1)
+          this.protagonist.moveLeft()           
+  
+      } else if(this.sketch.keyCode === this.sketch.RIGHT_ARROW) {
+          // Check if not going out of the map
+          if(this.protagonist.posX >= this.rows - 1) return false 
+          console.log(this.protagonist.posX)
+          console.log(this.rows - 1)
+          //element = this.getElement(this.sketch.keyCode, 1)
+          this.protagonist.moveRight()
+         // this.protagonist.swapSprite(this.sketch.keyCode, 0, 1)
+
+      } else if(this.sketch.keyCode === this.sketch.UP_ARROW) {
+          // Check if not going out of the map
+          if(this.protagonist.posY <= 0) return false 
+          //element = this.getElement(this.sketch.keyCode, 1)
+          this.protagonist.moveUp()
+
+      } else if(this.sketch.keyCode === this.sketch.DOWN_ARROW) {
+          // Check if not going out of the map
+          if(this.protagonist.posY >= this.columns-1) return false
+         // element = this.getElement(this.sketch.keyCode, 1)
+          this.protagonist.moveDown()
+
+      }
+      else{
+          return false
+      }
+      //element.action(keyCode)
+      //this.protagonist.swapSprite(keyCode, 0, 1)
+      this.refreshPos()
+      return true
   }
 
   /**
