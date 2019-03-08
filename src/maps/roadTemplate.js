794276848de4c9244f050c34f34168a4e1de8f3d
RICHOZ Julien <Julien.RICHOZ@cpnv.ch>
2019-03-08 13:47:00 +0100

    Add a road template if you want to design new level (all set to 9)

---
+++ src/maps/roadTemplate.js
@@ -0,0 +1,46 @@
+import { Boulder } from '../elements/Boulder'
+import { Door } from '../elements/Door'
+import { Protagonist } from '../elements/Protagonist'
+import { Pokeball } from '../elements/Pokeball'
+import { Road } from '../elements/Road'
+import { Tree } from '../elements/Tree'
+
+export default {
+    
+    /* 
+        0 = PlayableCharacter
+        1 = pokeball (objectif)
+        2 = door
+        3 = boulder
+        4 = tree
+        9 = road (case vide)
+    */
+   
+    pattern: [
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9],
+        [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9]
+    ],
+
+    e: {
+        0: Protagonist, //There can be only one protagonist in the pattern
+        1: Pokeball,
+        2: Door, //There can be only one Door actually
+        3: Boulder,
+        4: Tree,
+        9: Road
+    }
+}
