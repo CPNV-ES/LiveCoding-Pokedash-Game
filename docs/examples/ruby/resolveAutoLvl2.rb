d476e3461abfc8b9d909c34cef45feed73f8598c
JulienRichoz <julienrichoz@outlook.com>
2019-03-24 16:35:09 +0100

    Update solutions (ruby and php)

--- docs/examples/ruby/resolveAutoLvl2.rb
+++ docs/examples/ruby/resolveAutoLvl2.rb
@@ -1,12 +1,13 @@
-LIM_X = get_map_size_x.to_i
-LIM_Y = get_map_size_y.to_i
+LIM_X = get_map_size_x
+LIM_Y = get_map_size_y
 DIST = 1
 dir = 37 # Start direction = left
 element = nil
 out = false
 
 while !out do
-    if is_in_map(get_pos_x.to_i, get_pos_y.to_i, LIM_X, LIM_Y, dir, DIST) == 'true'
+    # If next move is in the map
+    if is_in_map(get_pos_x, get_pos_y, LIM_X, LIM_Y, dir, DIST) == 'true'
         element = get_element(dir, DIST)
         case element
         when 'Road'
@@ -28,6 +29,7 @@ while !out do
                 dir = 37
             end
         end
+    # If next move is out the map, change direction
     else
         dir += 1
         if dir > 40 
